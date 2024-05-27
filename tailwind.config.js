808fb565f5778e8f95a59076921d7c18e01d71e8
AJMAL M T <ajmalmt67@gmail.com>
2024-05-27 12:38:08 +0530

    c

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,16 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: [
-        "./pages/**/*.{js,ts,jsx,tsx,mdx,mjs}",
-        "./components/**/*.{js,ts,jsx,tsx,mdx,mjs}",
-        "./app/**/*.{js,ts,jsx,tsx,mdx,mjs}",
-    ],
-    theme: {
-        extend: {
-            backgroundImage: {
-                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-                "gradient-conic":
-                    "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-            },
+export const content = [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx,mjs}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx,mjs}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx,mjs}",
+];
+export const theme = {
+    extend: {
+        backgroundImage: {
+            "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+            "gradient-conic":
+                "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
         },
     },
-    plugins: [],
 };
+export const plugins = [];
